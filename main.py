12a1b83eccdca80e7adb201dca403ce91f2cbf9a
Subhodeep Chatterjee <143993347+Subhogenome@users.noreply.github.com>
2025-10-21 20:57:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class AgentState(BaseModel):
 
 
 # ---------- LLM Setup ----------
-api_key = ""
+api_key = st.secrets["api"]
 llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct", temperature=0, api_key=api_key)
 
 
